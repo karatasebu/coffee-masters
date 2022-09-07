66ab20bc93ba8ff15cb024c375d1bbf791de201f
karatasebu <karatasebu47@gmail.com>
2022-09-07 11:07:32 +0300

    Initial Commit

---
+++ CoffeeMasters/CoffeeMastersApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoffeeMastersApp.swift
+//  CoffeeMasters
+//
+//  Created by Ebubekir Karatas on 7.09.2022.
+//
+
+import SwiftUI
+
+@main
+struct CoffeeMastersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
